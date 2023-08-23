85011e0e8a956c7a8a93f7084df840db35cab5d2
Tuomas <tuomas.orhanen@amban.fi>
2023-08-23 14:43:33 +0300

    Add favicon and robot.txt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html className="dark" lang="en">
       <Head>
-        {/* Add the new Google Fonts link here */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&family=Open+Sans:wght@400;600&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
