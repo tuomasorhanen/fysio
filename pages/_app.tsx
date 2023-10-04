a7eec26a2a3dfc7d5b7e3601e1773fb07af3334f
Tuomas <tuomas.orhanen@amban.fi>
2023-10-04 12:39:05 +0300

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import 'styles/global.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
