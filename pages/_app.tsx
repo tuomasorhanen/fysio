730d1ef4a12287ec1a9fd9243d2b0e463fbbe855
Tuomas <tuomas.orhanen@amban.fi>
2023-10-04 13:08:20 +0300

    clarity again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,14 @@
 import 'styles/global.css';
-import { Helmet } from 'react-helmet';
-
+import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-          <Helmet>
-        <script type="text/javascript">{`
-          (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "j58gi0phkx");
-        `}</script>
-      </Helmet>
+      <Head>
+        <script type="text/javascript" src="/path/to/clarityScript.js"></script>
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
