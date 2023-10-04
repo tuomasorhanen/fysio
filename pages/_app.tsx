d6308cf98a476265e3a7a8fe87ac3d73a4efed03
Tuomas <tuomas.orhanen@amban.fi>
2023-10-04 13:14:45 +0300

    try agina

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import 'styles/global.css';
 import Head from 'next/head';
+import 'styles/global.css';
 import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 
@@ -7,7 +7,18 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <script type="text/javascript" src="src/_lib/clarityScript.js"></script>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "j58gi0phkx");
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
       <Analytics />
