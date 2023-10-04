377480b63e97d19832af015e20c2e2318a245ccb
Tuomas <tuomas.orhanen@amban.fi>
2023-10-04 13:20:40 +0300

    keep trying

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,18 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "j58gi0phkx");
-            `,
-          }}
-        />
+      <script id="microsoft-clarity"> {` (function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "j58gi0phkx"); `} </script>
       </Head>
       <Component {...pageProps} />
       <Analytics />
