c21775bbeb1a4b75a5b1b9a2f5d18e40be410694
Tuomas <tuomas.orhanen@amban.fi>
2023-10-04 13:39:44 +0300

    new approach

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,18 @@ import 'styles/global.css';
 import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 import Script from 'next/script';
+import { clarity } from 'react-microsoft-clarity';
+import { useEffect } from 'react';
 
-const clarityCode = `
-(function(c,l,a,r,i,t,y){
-  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "j58gi0phkx");
-`;
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    clarity.init('j58gi0phkx');
+  }, []);
+  
   return (
     <>
-      <Component {...pageProps} />
-      <Script id="ms-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: clarityCode }} />
+          <Component {...pageProps} />
       <Analytics />
     </>
   );
